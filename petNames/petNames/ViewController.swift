6ebca6215370a105125040ce0670b0ee0d4d131e
Rodrigo <rodrigoktarouco@gmail.com>
2021-10-08 15:37:55 -0300

    Localizable settings defined and running.

--- petNames/petNames/ViewController.swift
+++ petNames/petNames/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension String {
+    func localized() -> String {
+        return NSLocalizedString(self, tableName: "Localizable", bundle: .main, value: self, comment: self)
+    }
+}
