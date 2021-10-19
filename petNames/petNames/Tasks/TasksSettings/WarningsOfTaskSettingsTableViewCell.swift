1d995c25dc274a0c8dada7e062de73c0c82ca37f
Heitor Kunrath <heitorkunrath@gmail.com>
2021-10-19 13:27:50 -0300

    view almost done, only lacking proper adjustments regarding text types ( sd rounded) due to errors in trying to set them up .  ViewController created but not in use, resulting in the table view not having cells to display. Cell class already created

---
+++ petNames/petNames/Tasks/TasksSettings/WarningsOfTaskSettingsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  WarningsOfTaskSettingsTableViewCell.swift
+//  petNames
+//
+//  Created by Heitor Feijó Kunrath on 19/10/21.
+//
+
+import UIKit
+
+class WarningsOfTaskSettingsTableViewCell: UITableViewCell {
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var isOn: UISwitch!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
