a6b5eb567a325480789fc4d57bc7863643ce35b6
Heitor Kunrath <heitorkunrath@gmail.com>
2021-10-20 13:40:06 -0300

    heitor`s commit Fixing screen for rodrigo

--- petNames/petNames/Tasks/TasksSettings/TasksSettingsViewController.swift
+++ petNames/petNames/Tasks/TasksSettings/TasksSettingsViewController.swift
@@ -9,19 +9,37 @@ import UIKit
 
 class TasksSettingsViewController: UIViewController {
     @IBOutlet weak var taskSettingsTitleLabel: UILabel!
+    @IBOutlet weak var addNewAlertLabel: UILabel!
     @IBOutlet weak var repeatLabel: UILabel!
     @IBOutlet weak var observationsTitlelabel: UILabel!
+    @IBOutlet weak var taskTimesTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        taskTimesTableView.dataSource = self
+        taskTimesTableView.delegate = self
     }
-    /*
-    // MARK: - Navigation
+    
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension TasksSettingsViewController : UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    
+    
+    
 }
