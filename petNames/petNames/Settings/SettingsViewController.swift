5e525b0fdc8b616772cd35a9f19d285dcb52fdde
Heitor Kunrath <heitorkunrath@gmail.com>
2021-10-21 17:27:43 -0300

    started settings screen

--- petNames/petNames/Settings/SettingsViewController.swift
+++ petNames/petNames/Settings/SettingsViewController.swift
@@ -8,22 +8,33 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet weak var settingsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        settingsTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    
+}
+extension SettingsViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var  cell =  settingsTableView.dequeueReusableCell(withIdentifier: "AdjustmentsTableViewCell") as! AdjustmentsTableViewCell
+        
+        
+        
+        
+        return cell
+    }
+    
+    
+    
 }
