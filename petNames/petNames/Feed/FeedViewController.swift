0550bedfeb6e85c0bdaeaa3764846ac8bcbc3173
Heitor Kunrath <heitorkunrath@gmail.com>
2021-10-28 14:22:09 -0300

    feed screen continuation, first collectionView done

--- petNames/petNames/Feed/FeedViewController.swift
+++ petNames/petNames/Feed/FeedViewController.swift
@@ -8,13 +8,52 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-
+    
+    @IBOutlet weak var welcomeUserLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var doneTasksLabel: UILabel!
+    @IBOutlet weak var nextTaskLabel: UILabel!
+    @IBOutlet weak var myPetsLabel: UILabel!
+    
+    @IBOutlet weak var doneTasksFunImage: UIImageView!
+    @IBOutlet weak var tasksCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tasksCollectionView.dataSource = self
+        setUpFontStyle ()
+        doneTasksFunImage.layer.cornerRadius = 22
+    }
+    func setUpFontStyle () {
+        welcomeUserLabel.font = UIFont(name: "SFProRounded-Bold", size: 24)
+        dayLabel.font = UIFont(name: "SFProRounded-Medium", size: 14)
+        dayLabel.textColor = .white
+        doneTasksLabel.font = UIFont(name: "SFProRounded-Bold", size: 24)
+        doneTasksLabel.textColor = .white
+        nextTaskLabel.font = UIFont(name: "SFProRounded-Semibold", size: 20)
+        myPetsLabel.font = UIFont(name: "SFProRounded-Semibold", size: 20)
     }
 }
-
+extension FeedViewController : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == tasksCollectionView {
+            return 2
+        }
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == tasksCollectionView {
+            let cell = tasksCollectionView.dequeueReusableCell(withReuseIdentifier: "NextTaskCollectionViewCell", for: indexPath) as! NextTaskCollectionViewCell
+            cell.taskTimeLabel.text = "teste"
+            cell.petImage.image = UIImage(named: "pitty")
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    
+}
 extension String {
     func localized() -> String {
         return NSLocalizedString(self, tableName: "Localizable", bundle: .main, value: self, comment: self)
---
+++ petNames/petNames/Feed/feed cells/NextTaskCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  NextTaskCollectionViewCell.swift
+//  petNames
+//
+//  Created by Heitor Feijó Kunrath on 28/10/21.
+//
+
+import UIKit
+
+class NextTaskCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var petImage: UIImageView!
+    @IBOutlet weak var taskNameLabel: UILabel!
+    @IBOutlet weak var taskTimeLabel: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    @IBOutlet weak var auxView: UIView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        petImage.layer.cornerRadius = 15
+        auxView.layer.cornerRadius = 22
+        
+        setUpFontStyle()
+        auxView.backgroundColor = FeedModel.sharedFeedModel.getTaskColor(.medicine)
+    }
+    
+    func setUpFontStyle () {
+        taskNameLabel.font = UIFont(name: "SFProRounded-Semibold", size: 17)
+        taskTimeLabel.font = UIFont(name: "SFProRounded-Regular", size: 10)
+        
+    }
+    
+}
