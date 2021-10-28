1a0445b04a8870997626e4ee03d99888fbfbb1ee
Guilherme V.A <guibi@hotmail.com.br>
2021-10-28 16:45:36 -0300

    Updated FeedModel
    - Now, all the tasks names are standard in the code.

--- petNames/petNames/Feed/FeedModel.swift
+++ petNames/petNames/Feed/FeedModel.swift
@@ -11,13 +11,13 @@ import UIKit
 enum TaskTypes {
 case water
 case feeding
-case shower
-case playTime
-case takeAWalk
-case tosa // traduzir!!!!
+case wash
+case playtime
+case walk
+case groom
 case medicine
 case vet
-case personalized
+case custom
 }
 class FeedModel {
     
@@ -29,19 +29,19 @@ static let sharedFeedModel = FeedModel()
             return UIColor(red: 0.898, green: 1, blue: 1, alpha: 1)
         case .feeding :
             return UIColor(red: 1, green: 0.947, blue: 0.898, alpha: 1)
-        case .shower :
+        case .wash :
             return UIColor(red: 0.937, green: 0.898, blue: 1, alpha: 1)
-        case .playTime :
+        case .playtime :
             return UIColor(red: 1, green: 0.898, blue: 0.996, alpha: 1)
-        case .takeAWalk :
+        case .walk :
             return UIColor(red: 0.898, green: 1, blue: 0.908, alpha: 1)
         case .medicine :
             return UIColor(red: 1, green: 0.898, blue: 0.898, alpha: 1)
-        case .tosa : // traduzir!!!!
+        case .groom :
             return UIColor(red: 1, green: 0.978, blue: 0.898, alpha: 1)
         case .vet :
             return UIColor(red: 0.922, green: 0.922, blue: 0.922, alpha: 1)
-        case .personalized :
+        case .custom :
             return UIColor(red: 0.898, green: 0.949, blue: 1, alpha: 1)
         default:
             return UIColor(red: 0.898, green: 0.949, blue: 1, alpha: 1)
