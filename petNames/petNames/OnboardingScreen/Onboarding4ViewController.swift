ba3bb63f9144ce47342f2fb92dd49d58a3d1702f
Rodrigo Kroef tarouco <55713305+rodrigoktarouco@users.noreply.github.com>
2021-11-15 15:04:04 -0300

    Onboarding4 created and set it

--- petNames/petNames/OnboardingScreen/Onboarding4ViewController.swift
+++ petNames/petNames/OnboardingScreen/Onboarding4ViewController.swift
@@ -8,11 +8,40 @@
 import UIKit
 
 class Onboarding4ViewController: UIViewController {
+    @IBOutlet weak var onboarding4SignUpView: UIView!
+    @IBOutlet weak var onboarding4HeaderTitleLabel: UILabel!
+    @IBOutlet weak var onboarding4SubHeaderTitleLabel: UILabel!
+    @IBOutlet weak var onboarding4QuestionLabel: UILabel!
+    @IBOutlet weak var onboarding4TextBoxView: UITextField!
+    @IBOutlet weak var onboarding4SkipButton: UIButton!
+    @IBOutlet weak var onboarding4SaveButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        onboarding4SaveButton.layer.cornerRadius = 22
+        onboarding4SignUpView.round(corners: [.bottomLeft, .bottomRight, .topRight], radius: 90.0)
+        onboarding4HeaderTitleLabel.text = "onboarding4HeaderTitleLabel".localized()
+        onboarding4HeaderTitleLabel.text = "onboarding4SubHeaderTitleLabel".localized()
+        onboarding4QuestionLabel.text = "onboarding4QuestionLabel".localized()
+        onboarding4SkipButton.setTitle("onboarding3LetsStartButton".localized(), for: .normal)
+        onboarding4SaveButton.setTitle("onboarding4SaveButton".localized(), for: .normal)
 
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func onboarding4SkipDidPress(_ sender: UIButton) {
+    }
+    
+    @IBAction func onboarding4SaveDidPress(_ sender: Any) {
+    }
+
+}
+
+// MARK: Round View Corners
+extension UIView {
+  func round(corners: UIRectCorner, radius: CGFloat) {
+    let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    self.layer.mask = mask
+  }
 }
