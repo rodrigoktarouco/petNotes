110e1c5dee85a47486a1da83a61ac459d91c146b
HeitorKun <60748906+HeitorKun@users.noreply.github.com>
2021-12-06 14:43:11 -0300

    merged here for debugging

--- petNames/petNames/Tasks/TasksScreen/NoTaskPHTableViewCell.swift
+++ petNames/petNames/Tasks/TasksScreen/NoTaskPHTableViewCell.swift
@@ -48,7 +48,6 @@ class NoTaskPHTableViewCell: UITableViewCell {
         }
     }
 
-    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
